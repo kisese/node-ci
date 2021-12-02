14dda85eccd32161acaa3e32022c78e9f7285135
Brian Kisese <brian.kisese@roamtech.com>
2021-12-02 12:23:07 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const PORT = 3000;
+const app = express();
+app.get('/', (req, res) => {
+    res.send('Hello Guys!!');
+});
+app.listen(PORT);
+console.log(`Running on ${PORT}`);
